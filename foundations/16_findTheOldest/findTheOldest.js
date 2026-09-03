fcb447ba3d7889c1dd1063eb0afb1ca32e85fce8
uditv67 <241539114+Uditv67@users.noreply.github.com>
2026-09-03 12:46:26 +0000

    done 16

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
+const getAge = function(people) {
+    const endYear = people.yearOfDeath || new Date().getFullYear();
+    return endYear - people.yearOfBirth;
+}
 
+const findTheOldest = function(people) {
+    return people.reduce((oldest, current) => {
+        return getAge(current) > getAge(oldest) ? current : oldest;
+    });
 };
 
 // Do not edit below this line
