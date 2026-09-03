853fb2e936b4c35e4aafff69b2d063a77597c4bd
uditv67 <241539114+Uditv67@users.noreply.github.com>
2026-09-03 11:15:24 +0000

    done 14

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function(count) {
+    count = Number(count);
+    if(count === 0){
+        return 0;
+    }
+    if(count < 0){
+        return 'OOPS';
+    }
+    if(count === 1){
+        return 1;
+    }
+    let first = 0;
+    let second = 1;
+    let current = 0;
 
+    for(let i = 1; i < count; i++) {
+        current = first + second;
+        first = second;
+        second = current;
+    }
+
+    return current;
 };
 
 // Do not edit below this line
